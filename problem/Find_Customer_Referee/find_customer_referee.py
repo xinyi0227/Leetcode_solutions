7d0b07334767ce154c79502e198a3dd9a64f6521
xinyi0227 <1221200909@student.mmu.edu.my>
2025-07-12 01:31:58 +0800

    find_customer_referee

---
+++ problem/Find_Customer_Referee/find_customer_referee.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+
+    result = customer[(customer['referee_id'] != 2) | (customer['referee_id'].isna())]
+    
+    return result[['name']]
