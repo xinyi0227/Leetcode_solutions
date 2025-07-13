d2943764cc21b9d2a5682ab6507068e950f16001
xinyi0227 <1221200909@student.mmu.edu.my>
2025-07-14 04:09:16 +0800

    find_classes

---
+++ problem/Classes_5_Students/find_classes.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    df = courses.groupby(by='class').count().reset_index()
+    return df[df['student']>=5][['class']]
