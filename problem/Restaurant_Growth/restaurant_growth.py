f0a50da9728d12f95b6adb4afdbab538aec8adc6
xinyi0227 <1221200909@student.mmu.edu.my>
2025-07-23 17:44:00 +0800

    restaurant_growth

---
+++ problem/Restaurant_Growth/restaurant_growth.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def restaurant_growth(customer: pd.DataFrame) -> pd.DataFrame:
+    df = customer.sort_values("visited_on").groupby("visited_on")[["amount"]].sum()
+    df = df.assign(amount = df.rolling("7D").sum(), average_amount = round(df.rolling("7D").sum()/7, 2))
+    df = df.reset_index()
+
+    df = df[df["visited_on"]>=(df["visited_on"][0]+ pd.Timedelta(days=6))]
+    return df
