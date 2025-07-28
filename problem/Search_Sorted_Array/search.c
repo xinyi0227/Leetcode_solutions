ef675f126c317ad8221d8dcae1f520e3812f66a9
xinyi0227 <1221200909@student.mmu.edu.my>
2025-07-28 23:44:18 +0800

    search

---
+++ problem/Search_Sorted_Array/search.c
@@ -0,0 +1,39 @@
+int search(int *nums, int numsSize, int target)
+{
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] >= nums[left])
+        {
+            if (nums[left] <= target && target <= nums[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        else
+        {
+            if (nums[mid] <= target && target <= nums[right])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
