564e35ab047346a85510ce0d9da0252d8bb10de3
xinyi0227 <1221200909@student.mmu.edu.my>
2025-08-03 03:14:42 +0800

    canJump

---
+++ problem/Jump_Game/canJump.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums):
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return True if goal == 0 else False
+        
