e123ddc4b5ef986fe52e69741188fe9d13b656bf
xinyi0227 <1221200909@student.mmu.edu.my>
2025-08-17 14:22:27 +0800

    new21Game

---
+++ problem/New_21_Game/new21Game.c
@@ -0,0 +1,34 @@
+double new21Game(int n, int k, int maxPts)
+{
+    if (k == 0 || n >= k - 1 + maxPts)
+        return 1.0;
+
+    double *dp = (double *)calloc(maxPts, sizeof(double));
+    dp[0] = 1.0;
+
+    double windowSum = 1.0, result = 0.0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        double prob = windowSum / maxPts;
+
+        if (i < k)
+        {
+            windowSum += prob;
+        }
+        else
+        {
+            result += prob;
+        }
+
+        if (i >= maxPts)
+        {
+            windowSum -= dp[i % maxPts];
+        }
+
+        dp[i % maxPts] = prob;
+    }
+
+    free(dp);
+    return result;
+}
