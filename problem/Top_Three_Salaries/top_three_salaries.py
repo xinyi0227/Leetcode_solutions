46778b5c3561f8942fcb197c8e1ff7cef562fff0
xinyi0227 <1221200909@student.mmu.edu.my>
2025-10-27 23:12:50 +0800

    done

--- problem/Top_Three_Salaries/top_three_salaries.py
+++ problem/Top_Three_Salaries/top_three_salaries.py
@@ -4,6 +4,7 @@ def filter(row):
     row['rank'] = row["salary"].rank(method="dense",ascending=False)
     return row[row["rank"]<=3]
 
+
 def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
     merged = pd.merge(employee,department,left_on="departmentId",right_on="id",how="left")
     ans = merged.groupby(["name_y"])\
