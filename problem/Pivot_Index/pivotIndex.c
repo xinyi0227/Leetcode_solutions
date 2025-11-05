3191271a11bdce3a18e591c847e6b82a6728c575
xinyi0227 <1221200909@student.mmu.edu.my>
2025-11-05 13:50:44 +0800

    pivotIndex

---
+++ problem/Pivot_Index/pivotIndex.c
@@ -0,0 +1,15 @@
+int pivotIndex(int *nums, int numsSize)
+{
+    int rightSum = 0;
+    for (int i = 0; i < numsSize; i++)
+        rightSum += nums[i];
+    int leftSum = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        rightSum -= nums[i];
+        if (leftSum == rightSum)
+            return i;
+        leftSum += nums[i];
+    }
+    return -1;
+}
