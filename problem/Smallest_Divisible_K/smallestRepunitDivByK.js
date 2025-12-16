b3a263f83f7807bad18ed5c29464a0726aa623b7
xinyi0227 <1221200909@student.mmu.edu.my>
2025-12-16 23:21:38 +0800

    smallestRepunitDivByK

---
+++ problem/Smallest_Divisible_K/smallestRepunitDivByK.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRepunitDivByK = function(k) {
+    if(k % 2 == 0 || k % 5 == 0) return -1;
+    let rem = 1 % k, len = 1;
+    while(rem > 0) {
+        rem = (rem * 10 + 1) % k;
+        len++;
+        if(len > k) return -1;
+    }
+    return len;
+};
