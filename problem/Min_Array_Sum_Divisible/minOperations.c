66e40f738a06a38eb25d5fa74b7863516d78cac5
xinyi0227 <1221200909@student.mmu.edu.my>
2025-12-20 23:20:47 +0800

    minOperations

---
+++ problem/Min_Array_Sum_Divisible/minOperations.c
@@ -0,0 +1,9 @@
+int minOperations(int *nums, int numsSize, int k)
+{
+    long long sum = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        sum += nums[i];
+    }
+    return sum % k;
+}
---
+++ problem/Min_Array_Sum_Divisible/minOperations.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, int k)
+    {
+        long long sum = 0;
+        for (int x : nums)
+            sum += x;
+        return sum % k;
+    }
+};
