0f75350e30b2ab4a74505fd6b30684ae8020943a
xinyi0227 <1221200909@student.mmu.edu.my>
2025-12-23 22:26:50 +0800

    countOdds

---
+++ problem/Count_Odd_Numbers/countOdds.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    int countOdds(int low, int high)
+    {
+        int odd = (high - low) / 2;
+        if (low % 2 == 1 || high % 2 == 1)
+            odd++;
+        return odd;
+    }
+};
